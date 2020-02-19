5646a0553e021a6ee003d37b3e3a99a1c592a683
Krishana <krishanakhatri264@gmail.com>
2020-02-20 03:05:55 +0530

    Basic CRUD operations for todo app

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,30 +1,40 @@
-const Task = require('../models/tasks');
+const Task = require("../models/tasks");
 
+class HomeController {
+  home(request, response) {
+    Task.find({}, function(error, tasks) {
+      if (error) {
+        console.log("Error in fetching contacts");
+        return;
+      }
+      response.render("home", {
+        title: "ToDo",
+        tasks: tasks
+      });
+    });
+  }
 
-class HomeController{
-    home(request,response){
-        Task.find({},function(error,tasks){
-            if(error){
-                console.log('Error in fetching contacts');
-                return;
-            }
-            response.render('home',{
-                title: 'ToDo',
-                tasks: tasks
-            });
-        });
-    }
+  createTask(request, response) {
+    Task.create(request.body, function(error, new_task) {
+      if (error) {
+        console.log("Error in creating task");
+        return;
+      }
+      console.log(new_task);
+      response.redirect("/");
+    });
+  }
 
-    createTask(request,response){
-        Task.create(request.body, function(error,new_task){
-            if(error){
-                console.log('Error in creating task');
-                return;
-            }
-            console.log(new_task);
-            response.redirect('/');
-        });
-    }
+  deleteTask(request, response) {
+    const id = request.query.id;
+    Task.findByIdAndDelete(id, function(error) {
+      if (error) {
+        console.log("Error while deleteing contact");
+        return;
+      }
+      response.redirect("back");
+    });
+  }
 }
 
-module.exports = HomeController; 
+module.exports = HomeController;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-const HomeController = require('../controllers/HomeController');
+const HomeController = require("../controllers/HomeController");
 const homeController = new HomeController();
 
-router.get('/',homeController.home);
-router.post('/createTask',homeController.createTask);
+router.get("/", homeController.home);
+router.post("/createTask", homeController.createTask);
+router.get("/deleteTask", homeController.deleteTask);
 
 module.exports = router;
